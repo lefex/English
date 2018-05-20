0f8f0583b116e72f6ed6a07d1548cddcc7c8ec1c
Wang,Suyan <wangsuyan@baidu.com>
2018-05-20 09:59:46 +0800

    英语项目

---
+++ Engilish/Engilish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Engilish
+//
+//  Created by Wang,Suyan on 2018/5/20.
+//  Copyright © 2018年 Wang,Suyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Engilish/EngilishTests/EngilishTests.m
@@ -0,0 +1,39 @@
+//
+//  EngilishTests.m
+//  EngilishTests
+//
+//  Created by Wang,Suyan on 2018/5/20.
+//  Copyright © 2018年 Wang,Suyan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EngilishTests : XCTestCase
+
+@end
+
+@implementation EngilishTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
